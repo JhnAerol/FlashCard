9175cb04c11bb960d88878429c4496e710731e27
ACT-STUDENT <ACT-STUDENT@LAB1-PC12>
2025-10-08 11:58:43 +0800

    Add project files.

---
+++ FlashCard/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace FlashCard
+{
+    public partial class MainPage : ContentPage
+    {
+        Random random = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NumberCarousel.ItemsSource = new Dictionary<int, string>
+            {
+                {1, "How are you" },
+                {2, "How Old are you?" },
+                {3, "What is your name" },
+
+            };
+        }
+
+        private void OnShuffleClicked(object sender, EventArgs e)
+        {
+            // Pick random index
+            int index = random.Next(0, 3);
+
+            // Animate transition
+            NumberCarousel.Position = index;
+        }
+    }
+}
