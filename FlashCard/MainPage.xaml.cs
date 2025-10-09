81e6dad4ef000c46f7713d2c6142c1594d2d606b
AJ <AJ@DESKTOP-H4JIUJU>
2025-10-09 20:57:40 +0800

    goods

--- FlashCard/MainPage.xaml.cs
+++ FlashCard/MainPage.xaml.cs
@@ -2,28 +2,116 @@
 {
     public partial class MainPage : ContentPage
     {
-        Random random = new();
+        //Initializations
+        Random _random = new Random();
+        int TotalPokemon = 10;
 
         public MainPage()
         {
             InitializeComponent();
+            InitializePokedex();
 
-            NumberCarousel.ItemsSource = new Dictionary<int, string>
-            {
-                {1, "How are you" },
-                {2, "How Old are you?" },
-                {3, "What is your name" },
-
-            };
         }
 
+        //To Display Pokedex
+        private void InitializePokedex()
+        {
+            NumberCarousel.ItemsSource = Pokedex.pokedex;
+            NumberCarousel.Position = 0;
+        }
+
+        //Navigate Shuffle Items
         private void OnShuffleClicked(object sender, EventArgs e)
         {
-            // Pick random index
-            int index = random.Next(0, 3);
+            int randomIndex = _random.Next(0, TotalPokemon);
+            NumberCarousel.Position = randomIndex;
+            ClearAnswerField();
+        }
 
-            // Animate transition
-            NumberCarousel.Position = index;
+        //Navigate Next Item
+        private void OnNextClicked(object sender, EventArgs e)
+        {
+            NumberCarousel.Position = (NumberCarousel.Position == TotalPokemon - 1 ? NumberCarousel.Position = 0 : NumberCarousel.Position++);
+            ClearAnswerField();
+        }
+
+        //Navigate Prev Item
+        private void OnPrevClicked(object sender, EventArgs e)
+        {
+            NumberCarousel.Position = NumberCarousel.Position == 0 ? TotalPokemon - 1 : NumberCarousel.Position--;
+            ClearAnswerField();
+        }
+
+
+        //Sumbit Answer
+        private async void OnEnterClicked(object sender, EventArgs e)
+        {
+            //Prevent Null Answer
+            if (string.IsNullOrWhiteSpace(txtAnswer.Text))
+            {
+                await DisplayAlert("Empty Answer", "Please enter a Pokémon name!", "OK");
+                return;
+            }
+
+            string userInput = txtAnswer.Text.Trim();
+
+            //Check first if the CurrentItems is a KeyValuePair
+            if (NumberCarousel.CurrentItem is KeyValuePair<string, ImageClass> currentPokemon)
+            {
+                bool isCorrect = string.Equals(currentPokemon.Key, userInput, StringComparison.OrdinalIgnoreCase);
+
+                //Then check if the Answer is Correct or Incorrect
+                if (isCorrect)
+                {
+                    await ShowCorrectAnswer(currentPokemon.Key);
+                }
+                else
+                {
+                    await ShowIncorrectAnswer(currentPokemon.Key);
+                }
+            }
+        }
+
+        //Method to call if the answer is Correct
+        private async Task ShowCorrectAnswer(string pokemonName)
+        {
+            //Display alert when answer is Correct
+            await DisplayAlert(
+                "Correct!", $"That's right! It's {pokemonName}! ", "Next Pokémon"
+            );
+
+            //Go to next pokemon
+            if (NumberCarousel.Position <= TotalPokemon - 1)
+            {
+                if(NumberCarousel.Position == TotalPokemon - 1)
+                {
+                    NumberCarousel.Position = 0;
+                }
+                else
+                {
+                    NumberCarousel.Position++;
+                }
+            }
+
+            ClearAnswerField();
+        }
+
+        //Method to call if the answer is Incorrect
+        private async Task ShowIncorrectAnswer(string correctAnswer)
+        {
+            //Display alert when answer is Incorrect
+            await DisplayAlert(
+                "Incorrect", $"Sorry! Wromg amswer :(. Try again!", "Try Again"
+            );
+
+            ClearAnswerField();
+            txtAnswer.Focus();
+        }
+
+        //Clear input fields
+        private void ClearAnswerField()
+        {
+            txtAnswer.Text = string.Empty;
         }
     }
 }
---
+++ FlashCard/Pokedex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCard
+{
+    public class Pokedex
+    {
+        public static Dictionary<string, ImageClass> pokedex = new Dictionary<string, ImageClass>()
+        {
+            { "articuno", new ImageClass { ImageFile = "articuno.png" } },
+            { "caterpie", new ImageClass { ImageFile = "caterpie.png" } },
+            { "charmeleon", new ImageClass { ImageFile = "charmeleon.png" } },
+            { "eevee", new ImageClass { ImageFile = "eevee.png" } },
+            { "ivysaur", new ImageClass { ImageFile = "ivysaur.png" } },
+            { "jigglypuff", new ImageClass { ImageFile = "jigglypuff.png" } },
+            { "nidoran", new ImageClass { ImageFile = "nidoran.png" } },
+            { "pigeot", new ImageClass { ImageFile = "pigeot.png" } },
+            { "pikachu", new ImageClass { ImageFile = "pikachu.png" } },
+            { "wartortle", new ImageClass { ImageFile = "wartotle.png" } }
+        };
+    }
+}
